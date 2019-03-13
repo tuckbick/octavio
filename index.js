b02d299c989438a6683eeab6503500b9b9517b9b
Tucker Bickler <tbickler@homeaway.com>
2019-03-12 23:24:33 -0500

    DB thangs

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-"use strict";
+'use strict';
 
-const Hapi = require("hapi");
-const Boom = require("boom");
-
+const Hapi = require('hapi');
+const Boom = require('boom');
+const initDb = require('./initDb');
 
 const launchServer = async function() {
   const server = Hapi.server({ port: 8080 });
   const options = {
-    promises: "native",
-    uri: `mongodb+srv://erickflores:4F$o1h4MFOp4@cluster0-diryt.mongodb.net/test?retryWrites=true`,
+    promises: 'native',
+    // uri: `mongodb+srv://erickflores:4F$o1h4MFOp4@cluster0-diryt.mongodb.net/test?retryWrites=true`,
+    uri: 'mongodb://localhost:27017/test?retryWrites=true',
     mongooseOptions: { useNewUrlParser: true }
   };
 
   await server.register({
-    plugin: require("hapi-mongoose"),
+    plugin: require('hapi-mongoose'),
     options: options
   });
 
-  server.route({
-    method: "GET",
-    path: "/",
-    async handler(request) {
-      const db = server.plugins["hapi-mongoose"].connection;
-      const {ObjectID} = server.plugins["hapi-mongoose"].lib;
+  await initDb(server);
 
-      try {
-        const result = await db
-          .collection("users")
-          .findOne({ _id: new ObjectID(request.params.id) });
-        return result;
-      } catch (err) {
-        throw Boom.internal("Internal MongoDB error", err);
-      }
+  const db = server.plugins['hapi-mongoose'].connection;
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, h) {
+      return 'OK'
     }
   });
 
