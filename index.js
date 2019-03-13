0d25f588ca5e6157795c6c119a973b07c2eb0ed2
Erick F <erickflores785@gmail.com>
2019-03-12 22:29:10 -0500

    adding a change from the feature branch

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 // placeholder data
 const social_feed = require('./social_feed.json')
 
-const Hapi = require('hapi')
+const Hapi = require('hapi');
+const Boom = require('boom');
+const initDb = require('./initDb');
 
-;(async () => {
-  const server = Hapi.server({ port: 8080 })
+const launchServer = async function() {
+  const server = Hapi.server({ port: 8080 });
+  const options = {
+    promises: 'native',
+    // uri: `mongodb+srv://erickflores:4F$o1h4MFOp4@cluster0-diryt.mongodb.net/test?retryWrites=true`,
+    uri: 'mongodb://localhost:27017/test?retryWrites=true',
+    mongooseOptions: { useNewUrlParser: true }
+  };
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, h) {
-      return 'OK'
-    }
-  })
+  await server.register({
+    plugin: require('hapi-mongoose'),
+    options: options
+  });
+
+  await initDb(server);
+
+  const db = server.plugins['hapi-mongoose'].connection;
 
   server.route({
     method: 'GET',
@@ -25,5 +34,10 @@ const Hapi = require('hapi')
 
   await server.start()
 
-  console.log('Server running at:', server.info.uri)
-})()
+  await server.start();
+  console.log(`Server started at ${server.info.uri}`);
+};
+launchServer().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
