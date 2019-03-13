09c441ffe0d8861acfb9228e9320d6da601d8862
Tucker Bickler <tbickler@homeaway.com>
2019-03-12 19:49:55 -0500

    Merge create-react-app and Hapi server

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 'use strict'
 
 const Hapi = require('hapi')
-const Vision = require('vision')
-const Handlebars = require('handlebars')
 
 ;(async () => {
   const server = Hapi.server({ port: 8080 })
-
-  await server.register(Vision)
-
-  server.views({
-    engines: { hbs: Handlebars },
-    relativeTo: __dirname
-  })
 
   server.route({
     method: 'GET',
     path: '/',
     handler: function (request, h) {
-      return h.view('index')
+      return 'OK'
     }
   })
 
