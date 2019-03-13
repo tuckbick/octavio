5fbf4101b047171e5f9157d8ec4312c7b4804d0e
Tucker Bickler <tbickler@homeaway.com>
2019-03-12 19:35:19 -0500

    Routes and templates

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 'use strict'
 
-const Hapi = require('hapi');
+const Hapi = require('hapi')
+const Vision = require('vision')
+const Handlebars = require('handlebars')
 
-(() => {
-  const server = Hapi.server()
+;(async () => {
+  const server = Hapi.server({ port: 8080 })
 
+  await server.register(Vision)
 
+  server.views({
+    engines: { hbs: Handlebars },
+    relativeTo: __dirname
+  })
 
-  server.start()
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, h) {
+      return h.view('index')
+    }
+  })
+
+  await server.start()
+
+  console.log('Server running at:', server.info.uri)
 })()
